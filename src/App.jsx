aa6781fcc95b4c7a7a18c042a50b02adc8c07d32
AnttiVainikka <antti.vainikka36@gmail.com>
2026-02-03 06:22:06 +0200

    Enabled logging in and viewing blogs

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import Blogs from './components/Blogs'
+import Notification from './components/Notification'
+import Login from "./components/Login"
+import blogService from './services/blogs'
+import loginService from "./services/login"
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  return (
+    <div>
+      <Notification message={errorMessage}/>
+      {!user && <Login
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+      />}
+      {user && <Blogs blogs={blogs}/>}
+    </div>
+  )
+}
+
+export default App
